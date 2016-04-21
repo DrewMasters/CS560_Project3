5e5949c4ee9594a1ab11632819a503431a4c9443
Cloud User <ec2-user@ip-172-31-61-146.ec2.internal>
2016-04-21 09:10:06 -0400

    modified code to not need title file will use titles from the actual page

--- preprocesser.py
+++ preprocesser.py
@@ -1,13 +1,18 @@
 #!/usr/bin/python
 
 import xml.sax
+import re
+
+def lp(content):
+	#get links from text of page
+	return re.findall(	
+	
 
 class pp(xml.sax.ContentHandler):
 	def __init__(self):
-		self.title_list = []
-		file1=open('simplewiki-20160305-all-titles','r')
-		for l in file1:
-			self.title_list.append(l.rstrip())
+		self.title_list = {}
+		self.link_list = {}
+		self.title_count = 0
 		self.title = ""
 		self.flagtitle = False
 		self.flagcontent = False
@@ -22,18 +27,24 @@ class pp(xml.sax.ContentHandler):
 			self.flagcontent=True
 	
 	def endElement(self, name):
+		self.link_list[self.title]=lp(self.content)
 		self.flagtitle=False
 		self.flagcontent=False
+		self.content=""
 
 	def characters(self, content):
 		if self.flagtitle:
 			self.title=content
+			self.title_list[self.title]=self.title_count
+			self.link_list[self.title]=[]
+			self.title_count+=self.title_count
 			print self.title
 		elif self.flagcontent:
-			self.content=content
-			print "content"
-
-
+			self.content+=content
+	
+	def endDocument():
+		#generate adjacency list
+		print "finished document"
 
 parser=xml.sax.make_parser()
 parser.setContentHandler(pp())
