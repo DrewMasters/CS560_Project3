9c418d444733a91c2674f316ca3e241e815121e5
DrewMasters <drewmasters78@gmail.com>
2016-04-21 23:13:11 -0400

    regex and content parsing function work. Still need to write function to generate adjacency list

--- preprocesser.py
+++ preprocesser.py
@@ -6,33 +6,39 @@ import time
 
 def lp(content):
 	#get links from text of page
-	print "\ncontent"
-	print content
-	r=re.findall(r'\[\[(.+?)\]\] |\[\[(.+?)\|.+\]\]',content)	
-	print "\nregex results"
-	print r
-	time.sleep(3)
-	return r
+	r=re.findall(r'\[\[(.+?)\]\]',content)
+	#for loop removes links to files and links in the form of [[test|test page]]
+	tmp = []
+	for t in r:
+		tmp2 = t.split("|")
+		if not "File" in tmp2[0]: 
+			tmp.append(tmp2[0])
+	return tmp
 
 class pp(xml.sax.ContentHandler):
 	def __init__(self):
-		self.title_list = {}
+		self.title_list = []
 		self.link_list = {}
-		self.title_count = 0
 		self.title = ""
 		self.flagtitle = False
 		self.flagcontent = False
-		self.number = 0
 		self.adfile = open('adlist.txt','w')
 		self.content=""
+		file1=open('simplewiki-20160305-all-titles','r')
+		for l in file1:
+			self.title_list.append(l.strip())
 
 	def startElement(self, name, attrs):
+		#reads in the opening tag and sets flags for reading in stuff between tags
+		#if title tag set title flag to true
 		if name == "title":
 			self.flagtitle = True
+		#if text tag set content flag to true
 		elif name == "text":
 			self.flagcontent=True
 	
 	def endElement(self, name):
+		#parse content, generate adjacency list, and reset everything
 		if self.flagcontent:
 			self.link_list[self.title]=lp(self.content)
 			self.content=""
@@ -40,16 +46,12 @@ class pp(xml.sax.ContentHandler):
 		self.flagcontent=False
 
 	def characters(self, content):
+		#read in title if title tag
 		if self.flagtitle:
 			self.title=content
-			self.title_list[self.title]=self.title_count
-			self.title_count+=self.title_count
+		#read in content if content tag
 		elif self.flagcontent:
 			self.content+=content
-	
-	def endDocument():
-		#generate adjacency list
-		print "finished document"
 
 parser=xml.sax.make_parser()
 parser.setContentHandler(pp())
